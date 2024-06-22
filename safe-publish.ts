5b8d77d56c9fe64b0b7f047e4a141767c431e621
Miguel Diaz <diazmiiguel@gmail.com>
2024-06-22 11:52:35 +0200

    contract deployment

--- safe-publish.ts
+++ safe-publish.ts
@@ -5,15 +5,24 @@ import {
   OperationType,
 } from "@safe-global/safe-core-sdk-types";
 import Logger from "https://deno.land/x/logger@v1.1.6/logger.ts";
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
+
+const env = await load();
+
+const SIGNER_ADDRESS = env["SIGNER_ADDRESS"];
+const SIGNER_PK = env["SIGNER_PK"];
+const SAFE_ADDRESS = env["SAFE_ADDRESS"];
+const RPC_URL = env["RPC_URL"];
+const SEPOLIA_ID = 11155111n;
 
 const logger = new Logger();
 
 // * For some reason, SafeApiKit and the Safe classes cannot be correctly
-// * constructed from the backend. Using the `defaul` property seems to fix that 🤷🏽
+// * constructed from the server side. Using the `defaul` property seems to fix that, but it's a hack 🤷🏽
 if (import.meta.main) {
   logger.info("Initiating tx publish");
 
-  const apiKit = new SafeProtocol.default({ chainId: 11155111n });
+  const apiKit = new SafeProtocol.default({ chainId: SEPOLIA_ID });
   const protocolKitOwner = await Safe.default.init({
     provider: RPC_URL,
     signer: SIGNER_PK,
--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,19 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPk = vm.envUint("DEPLOYER_PK");
+
+        vm.startBroadcast(deployerPk);
+
+        Counter cc = new Counter();
+        console.logString(string(abi.encodePacked("Counter:", address(cc))));
+
+        vm.stopBroadcast();
     }
 }
