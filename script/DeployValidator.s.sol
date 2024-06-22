0d664be6671bd37f5e7bd493ba5900db1a355764
Miguel Diaz <diazmiiguel@gmail.com>
2024-06-22 22:59:24 +0200

    fixups for deploy

--- script/DeployValidator.s.sol
+++ script/DeployValidator.s.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract DeployValidator is Script {
     function isDeployed(string memory contractName) internal view returns(bool) {
-        return vm.envOr(contractName, address(0)) != address(0);
+        address contractAddress = vm.envOr(contractName, address(0));
+
+        return contractAddress != address(0);
     }
 }
